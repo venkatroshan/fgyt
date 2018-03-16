eb9ca1597ed8d0e7f4dae49259b0487b8c74ec2e
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-03-16 16:42:28 +0530

    Create gh.c

---
+++ gh.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char str[20];
+int l,i,sum=0;
+printf("Enter the string");
+scanf("%s",str);
+l=strlen(str);
+for(i=0;i<l;i++)
+{
+  if(str[i]=='@'||str[i]=='*'||str[i]=='.'||str[i]=='#'||str[i]=='&'||str[i]=='^')
+  {
+    sum=sum+1;
+  }
+}
+printf("Special character:%d",sum);
+return 0;
+}
